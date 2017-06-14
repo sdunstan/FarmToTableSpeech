e12ef686365e416530ee4bbb03e06aebeffa34e7
Steve Dunstan <steve_dunstan@yahoo.com>
2017-06-14 09:12:26 -0700

    initial app

---
+++ src/main/java/sf/hackday/farmtotable/voicepump/Application.java
@@ -0,0 +1,35 @@
+package sf.hackday.farmtotable.voicepump;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public Configuration getConfiguration() {
+        Configuration config = new Configuration();
+
+        config.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        config.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        config.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        return config;
+    }
+
+    @Bean
+    public LiveSpeechRecognizer getRecognizer(Configuration config) throws IOException {
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(config);
+        return recognizer;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/sf/hackday/farmtotable/voicepump/Controller.java
@@ -0,0 +1,53 @@
+package sf.hackday.farmtotable.voicepump;
+
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private LiveSpeechRecognizer recognizer;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    private boolean cache = false;
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/start")
+    public String start() {
+       recognizer.startRecognition(cache);
+       cache = false;
+       StringBuffer buf = new StringBuffer();
+       for(int i = 0; i < 3; i++) {
+           SpeechResult result = recognizer.getResult();
+           buf.append(result.getHypothesis()).append(" ");
+       }
+       recognizer.stopRecognition();
+
+       rabbitTemplate.convertAndSend("SPEECH-QUEUE", buf.toString());
+
+       return buf.toString();
+    }
+
+    @RequestMapping("/pause")
+    public String pause() {
+        recognizer.stopRecognition();
+        return "Recognizer paused.";
+    }
+
+    @RequestMapping("/resume")
+    public String resume() {
+        recognizer.startRecognition(false);
+        return "Recognizer paused.";
+    }
+}
